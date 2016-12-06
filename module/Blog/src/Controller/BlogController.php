60b9ef7d4b0568168226e08688962616e9a64351
Adriano A Costa <sdcomputadores@gmail.com>
2016-12-06 17:11:32 -0200

    Criação do CRUD e dos formulários

--- module/Blog/src/Controller/BlogController.php
+++ module/Blog/src/Controller/BlogController.php
@@ -26,4 +26,64 @@ class BlogController extends AbstractActionController
         ]);
     }
 
+    public function addAction()
+    {
+        $form = new PostForm();
+        $form->get('submit')->setValue('Caddastrar Post');
+        $request = $this->getRequest();
+        if (!$request->isPost()) {
+            return ['form' => $form];
+        }
+        $form->setData($request->getPost());
+        if (!$form->isValid()) {
+            return ['form' => $form];
+        }
+        $post = new Post();
+        $post->exchangeArray($form->getData());
+        $this->table->save($post);
+        return $this->redirect()->toRoute('post');
+    }
+
+    public function editAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('post');
+        }
+        try {
+            $post = $this->table->find($id);
+        } catch (\Exception $e) {
+            return $this->redirect()->toRoute('post');
+        }
+        $form = new PostForm();
+        $form->bind($post);
+        $form->get('submit')->setAttribute('value', 'Edit Post');
+        $request = $this->getRequest();
+        if (!$request->isPost()) {
+            return [
+                'id' => $id,
+                'form' => $form
+            ];
+        }
+        $form->setData($request->getPost());
+        if (!$form->isValid()) {
+            return [
+                'id' => $id,
+                'form' => $form
+            ];
+        }
+        $this->table->save($post);
+        return $this->redirect()->toRoute('post');
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('post');
+        }
+        $this->table->delete($id);
+        return $this->redirect()->toRoute('post');
+    }
+
 }
--- module/Blog/src/Model/PostTable.php
+++ module/Blog/src/Model/PostTable.php
@@ -32,4 +32,41 @@ class PostTable
         return $this->tableGateway->select();
     }
 
+    public function save(Post $post)
+    {
+        $data = [
+            'title' => $post->title,
+            'content' => $post->content
+        ];
+        $id = (int) $post->id;
+        if ($id === 0) {
+            $this->tableGateway->insert($data);
+            return;
+        }
+        if (!$this->find($id)) {
+            throw new RuntimeException(sprintf(
+                    'Não foi possível recuperar a linha %d', $id
+            ));
+        }
+        $this->tableGateway->update($data, ['id' => $id]);
+    }
+
+    public function find($id)
+    {
+        $id = (int) $id;
+        $rowset = $this->tableGateway->select(['id' => $id]);
+        $row = $rowset->current();
+        if (!$row) {
+            throw new RuntimeException(sprintf(
+                    'Could not retrieve the row %d', $id
+            ));
+        }
+        return $row;
+    }
+
+    public function delete($id)
+    {
+        $this->tableGateway->delete(['id' => (int) $id]);
+    }
+
 }
